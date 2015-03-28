6671d270a376e7929ef3dfb641878465580600ac
Matthew X. Economou <xenophon@irtnog.org>
2015-03-27 20:03:18 -0400

    Began writing a SWATH script that would "moth" a sector (purposefully enter with a weak/inexpensive ship that would help drain the fuel reserves of the Q-cannons located there).

---
+++ src/MothSector.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2012, Matthew X. Economou <xenophon@irtnog.org>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+import com.swath.UserDefinedScript;
+
+public class MothSector extends UserDefinedScript {
+	@Override
+	public String getName() {
+		return "Moth Sector";
+	}
+
+	@Override
+	public boolean initScript() throws Exception {
+		return true;
+	}
+
+	@Override
+	public boolean runScript() throws Exception {
+		// TODO Auto-generated method stub
+		return super.runScript();
+	}
+}
